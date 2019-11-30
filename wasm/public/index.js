28a171f65a9130c75ae853d8d050c6c806613475
Jeremy Stucki <jeremy@myelin.ch>
2019-11-30 14:36:13 +0100

    Make @bash happy

--- wasm/public/index.js
+++ wasm/public/index.js
@@ -1,24 +1,19 @@
-const js = import("./node_modules/pixel-perfect/pixel_perfect_wasm.js");
+const js = import('./node_modules/pixel-perfect/pixel_perfect_wasm.js');
 
 js.then(js => {
-    document.getElementById("button").onclick = function() {
-        let reader = new FileReader();
-        reader.onload = function() {
-            let pixelSize = document.getElementById("pixel-size").valueAsNumber;
+    document.getElementById('button').addEventListener('click', () => {
+        let pixelSize = document.getElementById('pixel-size').valueAsNumber;
+        document.getElementById('file').files[0].arrayBuffer().then((arrayBuffer) => {
+            let uint8Array = new Uint8Array(arrayBuffer);
+            let output = isNaN(pixelSize) ? js.export_normally(uint8Array) : js.force_export(uint8Array, pixelSize);
 
-            let uint8Array = new Uint8Array(this.result);
-            let output = pixelSize.isNaN ? js.export_normally(uint8Array) : js.force_export(uint8Array, pixelSize);
-
-            let a = window.document.createElement("a");
-            a.href = window.URL.createObjectURL(new Blob([output], { type: "image/png" }));
-            a.download = "image.png";
+            let a = document.createElement('a');
+            a.href = URL.createObjectURL(new Blob([output], { type: 'image/png' }));
+            a.download = 'image.png';
 
             document.body.appendChild(a);
             a.click();
             document.body.removeChild(a);
-        };
-
-        let file = document.getElementById("file").files[0];
-        reader.readAsArrayBuffer(file);
-    };
+        });
+    });
 });
